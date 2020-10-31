14352b708c968bcbcecedd2002da735f7c5b7499
jeojw <53222593+jeojw@users.noreply.github.com>
2020-10-31 18:44:22 +0900

    Update question.cpp

--- question.cpp
+++ question.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+bool isPrimeNumber(int n){
+	if (n == 2)
+		return true;
+	if (n == 1 || n % 2 ==  0)
+		return false;
+	
+	for (int i = 3; i < n; i += 2){
+		if (n % i == 0)
+			return false;
+	}
+	
+	return true;//->첫번째 소수판별법(n % 2)
+}
 
 int Goldbach(int number){
 	static vector<int> primes {3, 5, 7, 11, 13};
