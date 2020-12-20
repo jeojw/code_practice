58986b86a0b8739c186f8264765d89f0cb436c15
jeojw <53222593+jeojw@users.noreply.github.com>
2020-12-20 17:41:31 +0900

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -1,129 +1,68 @@
-#ifndef ADDER_H 
-#define ADDER_H
+#include "ListGraph.h"
 
 template <typename T>
-struct Node{
-	T data;
-	Node<T>* next;
-};
-
-template <typename T>
-class Stack{
-private:
-	Node<T>* First;
-	int size;
-	
-public:
-	Stack();
-	~Stack();
-	
-	bool IsEmpty() const;
-	int GetSize() const;
-	int Push(const T& data);
-	int Pop();
-	int Peek();
-	Node<T>* Search(int Index);
-	void Print();
-};
-
-#endif
-
-////////////////////////////////////////////
-
-#include <iostream>
-#include "Stack.h"
-using namespace std;
-
-template <typename T>
-Stack<T>::Stack(){
-	First = NULL;
-	size = 0;
+ListGraph<T>::ListGraph(){
+	edges = 0;
+	vertexs = 0;
 }
 
 template <typename T>
-Stack<T>::~Stack(){}
+ListGraph<T>::~ListGraph(){}
 
 template <typename T>
-bool Stack<T>::IsEmpty() const{
-	if (size == 0)
-		return true;
-	else
-		return false;
+int ListGraph<T>::GetEdge() const{
+	return edges;
 }
 
 template <typename T>
-int Stack<T>::GetSize() const{
-	return size;
+int ListGraph<T>::GetVertex() const{
+	return vertexs;
 }
 
 template <typename T>
-int Stack<T>::Push(const T& data){
-	Node<T>* newNode = new Node<T>;
-	newNode->data = data;
-	newNode->next = NULL;
+T ListGraph<T>::GetHeader(int Index) const{
+	return List[Index][0];
+}
+
+template <typename T>
+int ListGraph<T>::GetSize(int Index) const{
+	return List[Index].size();
+}
+
+template <typename T>
+T ListGraph<T>::RetreiveList(int Index1, int Index2){
+	return List[Index1][Index2];
+}
+
+template <typename T>
+int ListGraph<T>::PushHeader(const T& data){
+	vector<T> headlist;
+	headlist.push_back(data);
+	List.push_back(headlist);
+	vertexs++;
 	
-	if (size == 0)
-		First = newNode;
+	return 1;
+}
+
+template <typename T>
+int ListGraph<T>::LinkVertex(int Index1, int Index2){
+	List[Index1].push_back(List[Index2]);
+	List[Index2].push_back(List[Index2]);
 	
-	else{
-		Node<T>* cur = First;
-		while (cur->next != NULL)
-			cur = cur->next;
+	sort(List[Index1].begin(), List[Index1].end());
+	List[Index1].erase(unique(List[Index1].begin(), List[Index1].end()),List[Index1].end());
+	sort(List[Index2].begin(), List[Index2].end());
+	List[Index2].erase(unique(List[Index2].begin(), List[Index2].end()),List[Index2].end());
 		
-		cur->next = newNode;
-	}
-	
-	size++;
-	
 	return 1;
 }
 
 template <typename T>
-int Stack<T>::Pop(){
-	if (size == 0){
-		cout << "Stack is Empty!!" << endl;
-		return -1;
+void ListGraph<T>::PrintAll(){
+	for (int i = 0; i < List.size(); i++){
+		for (int j = 0; j < List[i].size(); j++){
+			cout << List[i][j] << "->";
+		}
+		cout << endl << "↓" << endl;
 	}
-	
-	Node<T>* prev = Search(size - 1);
-	Node<T>* del = prev->next;
-	
-	prev->next = NULL;
-	delete del;
-	
-	size--;
-	
-	return 1;
-}
-
-template <typename T>
-int Stack<T>::Peek(){
-	Node<T>* cur = First;
-	
-	while (cur->next != NULL)
-		cur = cur->next;
-
-	cout << cur->data << " -> ";
-	
-	return 1;
-}
-
-template <typename T>
-Node<T>* Stack<T>::Search(int Index){
-	Node<T>* cur = First;
-	for (int i = 0; i < Index - 1; i++)
-		cur = cur->next;
-	
-	return cur;
-}
-
-template <typename T>
-void Stack<T>::Print(){
-	Node<T>* cur = First;
-	
-	while (cur!= NULL){
-		cout << cur->data << " -> ";
-		cur = cur->next;
-	}
-		
 }
