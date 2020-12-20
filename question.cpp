a422972d04fdcd0e40c22e54390fd0f78a7d25e6
jeojw <53222593+jeojw@users.noreply.github.com>
2020-12-20 17:40:39 +0900

    Update question.cpp

--- question.cpp
+++ question.cpp
@@ -1,38 +1,3 @@
-#include <iostream>
-#include "ListGraph.h"
-#include "Stack.h"
-using namespace std;
-
-/*template <typename T>
-void DFS(ListGraph<T> graph, Stack<T> stack){
-	int visit[];
-	stack.Push(graph.GetHeader(0))
-}*/
-
-int main(){
-	ListGraph<int> graph;
-//	Stack<int> stack;
-	
-	graph.PushHeader(4);
-	graph.PushHeader(7);
-	graph.PushHeader(3);
-	graph.PushHeader(6);
-	graph.PushHeader(8);
-	graph.LinkVertex(0, 7);
-	graph.LinkVertex(0, 3);
-	graph.LinkVertex(1, 6);
-	graph.LinkVertex(2, 8);
-	graph.LinkVertex(3, 8);
-	graph.LinkVertex(3, 4);
-	graph.LinkVertex(4, 7);
-	graph.PrintAll();
-	
-//	void DFS(graph, stack);
-	
-	return 0;
-}
-///////////////////////////////////////////////////////
-
 #ifndef ADDER_H 
 #define ADDER_H
 #include <iostream>
@@ -50,87 +15,14 @@ public:
 	ListGraph();
 	~ListGraph();
 	
-	int GetEdge() const;
-	int GetVertex() const;
-	int GetHeader(int Index) const;
-	int GetSize(int Index) const;
-	int RetreiveList(int Index1, int Index2);
+	int GetEdge() const; // 그래프의 총 간선 개수
+	int GetVertex() const; // 그래프의 총 정점 개수
+	T GetHeader(int Index) const; // 그래프 헤더정점을 반환하는 함수
+	int GetSize(int Index) const; // 각 인접리스트의 크기를 반환하는 함수
+	T RetreiveList(int Index1, int Index2); // 각 인접리스트의 요소를 반환하는 함수
 	int PushHeader(const T& data); // 정점을 추가시키는 함수
-	int LinkVertex(int Index, const T& data);
-	void PrintAll();
+	int LinkVertex(int Index1, int Index2); // 헤더정점에서 갈 수 있는 정점을 추가시키는 함수
+	void PrintAll(); // 그래프 형태를 전원 출력시키는 함수
 };
 
 #endif
-
-///////////////////////////////////////////////////////////////////////
-
-#include "ListGraph.h"
-
-template <typename T>
-ListGraph<T>::ListGraph(){
-	edges = 0;
-	vertexs = 0;
-}
-
-template <typename T>
-ListGraph<T>::~ListGraph(){}
-
-template <typename T>
-int ListGraph<T>::GetEdge() const{
-	return edges;
-}
-
-template <typename T>
-int ListGraph<T>::GetVertex() const{
-	return vertexs;
-}
-
-template <typename T>
-int ListGraph<T>::GetHeader(int Index) const{
-	return List[Index][0];
-}
-
-template <typename T>
-int ListGraph<T>::GetSize(int Index) const{
-	return List[Index].size();
-}
-
-template <typename T>
-int ListGraph<T>::RetreiveList(int Index1, int Index2){
-	return List[Index1][Index2];
-}
-
-template <typename T>
-int ListGraph<T>::PushHeader(const T& data){
-	vector<T> headlist;
-	headlist.push_back(data);
-	List.push_back(headlist);
-	vertexs++;
-	
-	return 1;
-}
-
-template <typename T>
-int ListGraph<T>::LinkVertex(int Index, const T& data){
-	for (int i = 0; i < List.size(); i++){
-		if (List[i][0] != data)
-			continue;
-		
-		else{
-			List[Index].push_back(data);
-			edges++;
-			
-			return 1;
-		}
-	}
-}
-
-template <typename T>
-void ListGraph<T>::PrintAll(){
-	for (int i = 0; i < List.size(); i++){
-		for (int j = 0; j < List[i].size(); j++){
-			cout << List[i][j] << "->";
-		}
-		cout << endl << "↓" << endl;
-	}
-}
