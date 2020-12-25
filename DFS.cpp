f0f943ab7827245a0b5f0bc4309f3d6e5b34576e
jeojw <53222593+jeojw@users.noreply.github.com>
2020-12-25 16:45:21 +0900

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,57 +1,57 @@
-#i#include <iostream>
+#include <iostream>
+#include <stack>
 #include "ListGraph.h"
 #include "ListGraph.cpp"
-#include <stack>
 using namespace std;
 
 template <typename T>
-void DFS(int Index, ListGraph<T> graph){
-	stack<T> s;
-	bool visit[graph.GetVertex()] = { false };
+void DFS(T start, ListGraph<T> graph){
+	stack<T> s; // 스택
+	bool visit[graph.GetVertex()] = { false }; // 노드에 방문했는지 체크하는 배열
 	T seq[graph.GetVertex()]; // 노드 방문 순서를 기록하는 배열
 	
-	visit[Index] = true;
-	T cur = graph.GetHeader(Index);
+	int curIndex = graph.RetreiveIndex(start);
+	visit[curIndex] = true;
+	T cur = start;
 	s.push(cur);
-	int curIndex = Index;
-	int pos = 1;
-	int sequence = 0;
-	int pIndex;
+	int pos = 1; // 해당 노드에 연결되어있는 노드들
+	int sequence = 0; // 탐색 순서
+	int truevisit = 1; // 총 방문한 노드 개수
 	seq[sequence] = cur;
 	
-	while (true){
+	while (truevisit != graph.GetVertex()){
 		while (true){
-			if (visit[graph.RetreiveIndex(graph.RetreiveList(curIndex, pos))] == true){
+			if (visit[graph.RetreiveIndex(graph.RetreiveList(curIndex, pos))] == true){ // 해당 노드에 이미 방문했을경우 다른 인접노드 방문 및 백트래킹
 				pos++;
 				if (pos == graph.GetSize(curIndex)){
-					curIndex = pIndex;
+					s.pop();
+					curIndex = graph.RetreiveIndex(s.top());
+					pos = 1;
 				}
 			}
 			
-			else{
+			else{ // 노드 방문 성공 및 위의 변수들 갱신
 				cur = graph.RetreiveList(curIndex, pos);
-				pIndex = curIndex;
 				curIndex = graph.RetreiveIndex(cur);
 				s.push(cur);
 				visit[curIndex] = true;
 				pos = 1;
 				sequence++;
 				seq[sequence] = cur;
+				truevisit++;
 				break;
 			}
 		}
-		
-		if (s.size() == graph.GetVertex())
-			break;
-		else
-			continue;
 	}
+	int size = s.size();
+	for (int i = 0; i < size; i++) // 스택 내의 모든 요소를 제거함으로써 탐색 종료
+		s.pop();
 	
-	for (int i = 0; i < s.size(); i++)
+	for (int i = 0; i < truevisit; i++)
 		cout << seq[i] << "->";
 }
 
-int main(){
+/*int main(){
 	ListGraph<int> graph;
 	
 	graph.PushHeader(2);
@@ -72,7 +72,7 @@ int main(){
 	graph.LinkVertex(5, 6);
 	graph.LinkVertex(6, 1);
 	
-	DFS<int>(0, graph);
+	DFS<int>(8, graph);
 	
 	return 0;
-}
+}*/
