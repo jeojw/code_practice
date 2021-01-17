ff01d63f935ea52fa914bfb069d464162dca0879
jeojw <53222593+jeojw@users.noreply.github.com>
2021-01-17 17:38:17 +0900

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -7,47 +7,45 @@ using namespace std;
 template <typename T>
 void DFS(T start, ListGraph<T> graph){
 	stack<T> s; // 스택
-	bool visit[graph.GetVertex()] = { false }; // 노드에 방문했는지 체크하는 배열
-	T seq[graph.GetVertex()]; // 노드 방문 순서를 기록하는 배열
+	bool visit[graph.TotalVertex()] = { false }; // 노드에 방문했는지 체크하는 배열
+	T seq[graph.TotalVertex()] = { 0 }; // 노드 방문 순서를 기록하는 배열
 	
 	int curIndex = graph.RetreiveIndex(start);
-	visit[curIndex] = true;
 	T cur = start;
 	s.push(cur);
 	int pos = 1; // 해당 노드에 연결되어있는 노드들
 	int sequence = 0; // 탐색 순서
-	int truevisit = 1; // 총 방문한 노드 개수
-	seq[sequence] = cur;
 	
-	while (truevisit != graph.GetVertex()){
+	while (!s.empty()){
+		seq[sequence] = s.top(); // 스택 최상단의 노드를 순서에 넣음
+		sequence++;
+		visit[curIndex] = true;
+		
 		while (true){
-			if (visit[graph.RetreiveIndex(graph.RetreiveList(curIndex, pos))] == true){ // 해당 노드에 이미 방문했을경우 다른 인접노드 방문 및 백트래킹
-				pos++;
-				if (pos == graph.GetSize(curIndex)){
-					s.pop();
-					curIndex = graph.RetreiveIndex(s.top());
-					pos = 1;
-				}
-			}
-			
-			else{ // 노드 방문 성공 및 위의 변수들 갱신
+			if (visit[graph.RetreiveIndex(graph.RetreiveList(curIndex, pos))] != true){ // 방문하지 않은 노드일 경우 갱신함
 				cur = graph.RetreiveList(curIndex, pos);
-				curIndex = graph.RetreiveIndex(cur);
 				s.push(cur);
-				visit[curIndex] = true;
-				pos = 1;
-				sequence++;
-				seq[sequence] = cur;
-				truevisit++;
+				curIndex = graph.RetreiveIndex(s.top());
 				break;
 			}
+			else{
+				pos++;
+				if (pos == graph.GetSize(curIndex)){
+					s.pop(); // 인접리스트 내에 더이상 인접노드가 없는 경우 스택에서 노드 제거
+					if (!s.empty()){ // 스택이 비지 않은 경우에 인덱스 최신화
+						curIndex = graph.RetreiveIndex(s.top());
+						pos = 1;
+					}
+					else// 아닌 경우 반복문 종료
+						break;
+				}
+			}
 		}
+		
+		pos = 1; // 인접리스트의 처음부터 다시 탐색
 	}
-	int size = s.size();
-	for (int i = 0; i < size; i++) // 스택 내의 모든 요소를 제거함으로써 탐색 종료
-		s.pop();
 	
-	for (int i = 0; i < truevisit; i++)
+	for (int i = 0; i < graph.TotalVertex(); i++)
 		cout << seq[i] << "->";
 }
 
@@ -62,17 +60,13 @@ void DFS(T start, ListGraph<T> graph){
 	graph.PushHeader(8);
 	graph.PushHeader(10);
 	
-	graph.LinkVertex(0, 1);
-	graph.LinkVertex(0, 2);
-	graph.LinkVertex(0, 6);
-	graph.LinkVertex(1, 3);
-	graph.LinkVertex(2, 5);
-	graph.LinkVertex(3, 4);
-	graph.LinkVertex(3, 5);
-	graph.LinkVertex(5, 6);
-	graph.LinkVertex(6, 1);
+	graph.Link(0, 1);
+	graph.Link(0, 2);
+	graph.Link(1, 6);
+	graph.Link(3, 4);
+	graph.Link(4, 5);
 	
-	DFS<int>(8, graph);
+	DFS<int>(2, graph);
 	
 	return 0;
 }*/
