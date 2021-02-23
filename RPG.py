45ef87a33274a8aa30383445490785e0bedc5b46
jeojw <53222593+jeojw@users.noreply.github.com>
2021-02-23 23:56:24 +0900

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+pygame.init() # pygame 초기화
+
 temp_h = -150
 temp_t = 60
 JUMP_SPEED = 60 * temp_h / temp_t
@@ -12,6 +14,7 @@ MAP_LIMIT_RIGHT = 800
 
 LEFT = 'LEFT'
 RIGHT = 'RIGHT'
+
 class Bubble(object):
     def __init__(self, x_pos, y_pos, ATK, direction):
         self.x_pos = x_pos
@@ -19,7 +22,7 @@ class Bubble(object):
         self.ATK = ATK
         self.SPEED = 3
         self.direction = direction
-        image = pygame.image.load('bubble.png')
+        image = pygame.image.load('char_sprite/bubble.png')
         self.image = pygame.transform.scale(image, (30, 30))
         self.hitbox = self.image.get_rect(topleft=(self.x_pos, self.y_pos))
         
@@ -41,18 +44,15 @@ class Bubble(object):
 class Item(object):
     def __init__(self):
         pass
-        
-class Life(object): #통합 클래스로, 아래에 있는 클래스들의 부모 클래스 -> 쓸데없는 코드 길이 줄이는용
-    pass
-
 class Player(object):
     def __init__(self, x_pos, y_pos=None):
-        self.HP = 1000
-        self.ATK = 30
-        self.DEF = 20
-        self.SPEED = 10
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.HP = 1000
+        self.ATK = 500
+        self.DEF = 20
+        self.SPEED = 10
+        self.GETATTACK = 80 #피격당할 시 넉백되는 거리를 설정
 
         self.direction = RIGHT
         self.isDead = False
@@ -60,17 +60,17 @@ class Player(object):
         self.isWalk = False
         self.isOnGround = True
         self.isGetattack = False
+        self.isHitbox = True #사망 시에 히트박스 없는 것으로 처리
         self.index = 0
         self.cur = 0
         self.projectilelist = []
+        self.items = 0
         
-        self.atkcool = 60
-        
-        rightstatic = [pygame.image.load('char_static.png')]
-        rightdead = [pygame.image.load('char_dead.png')]
-        rightwalk = [pygame.image.load('char_walk_' + str(i) + '.png') for i in range(1, 4)]
-        rightattack = [pygame.image.load('char_attack.png')]
-        rightgetattack = [pygame.image.load('char_get_attack.png')]
+        rightstatic = [pygame.image.load('char_sprite/char_static.png')]
+        rightdead = [pygame.image.load('char_sprite/char_dead.png')]
+        rightwalk = [pygame.image.load('char_sprite/char_walk_' + str(i) + '.png') for i in range(1, 4)]
+        rightattack = [pygame.image.load('char_sprite/char_attack.png')]
+        rightgetattack = [pygame.image.load('char_sprite/char_get_attack.png')]
         leftwalk = [pygame.transform.flip(rightwalks, True, 0) for rightwalks in rightwalk]
         leftstatic = [pygame.transform.flip(rightstatic[0], True, 0)]
         leftdead = [pygame.transform.flip(rightdead[0], True, 0)]
@@ -82,6 +82,9 @@ class Player(object):
         self.curlist = self.rightlist
         self.cursprite = self.curlist[self.cur][self.index]
         self.hitbox = self.cursprite.get_rect(topleft=(self.x_pos, self.y_pos))
+        
+    def InitPos(self):
+        self.y_pos = MAP_GROUND - self.hitbox.height
         
     def SetStat(self, HP, ATK, DEF, SPEED):
         self.HP = HP
@@ -126,22 +129,17 @@ class Player(object):
             self.projectilelist.append(Bubble(self.hitbox.left, self.hitbox.y, self.ATK, LEFT))
         else:
             self.projectilelist.append(Bubble(self.hitbox.right, self.hitbox.y, self.ATK, RIGHT))
-        
-    def getattack(self):
-        self.isGetattack = True
-        self.isWalk = False
-        self.isAttack = False
-        
-        for bubble in self.projectilelist:
-            if (self.hitbox.right >)
 
     def notWalk(self):
         self.isWalk = False
         
     def notAttack(self):
         self.isAttack = False
-
-    def draw(self):
+        
+    def getItem(self, Item):
+        pass
+    
+    def drawpos(self):
         Screen.blit(self.cursprite, (self.hitbox.x, self.hitbox.y))
 
     def drawStat(self):
@@ -151,6 +149,10 @@ class Player(object):
         write('ATK: ' + str(self.ATK), BLACK, 70, 60)
         write('DEF: ' + str(self.DEF), BLACK, 70, 100)
         write('SPEED: ' + str(self.SPEED), BLACK, 85, 140)
+        
+    def draw(self):
+        self.drawpos()
+        self.drawStat()
         
     def updatepos(self):
         self.hitbox.x = self.x_pos
@@ -171,8 +173,25 @@ class Player(object):
                 self.x_pos += -self.SPEED
             elif (self.direction == RIGHT):
                 self.x_pos += self.SPEED
+        
+        for Enemy in Enemylist:
+            if (self.isHitbox is True):
+                if (self.checkcollision(Enemy) is True):
+                    self.isGetattack = True
+                    self.isWalk = False
+                    self.isAttack = False
+                    if (self.x_pos > Enemy.x_pos):
+                        self.direction = LEFT
+                        self.x_pos += self.GETATTACK
+                    else:
+                        self.direction = RIGHT
+                        self.x_pos -= self.GETATTACK
+                    
+        if (self.isDead is True):
+            self.GETATTACK = 0
+            self.isHitbox = False
 
-    def updatesprite(self):
+    def updatesprite(self): # 추후 아이템 획득시에도 스프라이트 관련 업데이트를 추가할 것
         if (self.direction == LEFT):
             self.curlist = self.leftlist
         elif (self.direction == RIGHT):
@@ -195,16 +214,171 @@ class Player(object):
             self.index = 0
         
         self.cursprite = self.curlist[self.cur][self.index]
+        
+    def update(self):
+        self.updatepos()
+        self.updatesprite()
 
 class Enemy(object):
-    def __init__(self):
-        pass
+    def __init__(self, x_pos, y_pos=None):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.HP = 2500
+        self.ATK = 60
+        self.DEF = 40
+        self.SPEED = 4
+        self.GETATTACK = 80 #피격당할 시 넉백되는 거리를 설정
+
+        self.direction = LEFT
+        self.isDead = False
+        self.isAttack = False
+        self.isWalk = False
+        self.isOnGround = True
+        self.isGetattack = False
+        self.isHitbox = True
+        self.index = 0
+        self.cur = 0
+        
+        rightstatic = [pygame.image.load('enemy_sprite/enemy_static.png')]
+        rightdead = [pygame.image.load('char_sprite/char_dead.png')]
+        rightwalk = [pygame.image.load('char_sprite/char_walk_' + str(i) + '.png') for i in range(1, 4)]
+        rightattack = [pygame.image.load('char_sprite/char_attack.png')]
+        rightgetattack = [pygame.image.load('char_sprite/char_get_attack.png')]
+        leftwalk = [pygame.transform.flip(rightwalks, True, 0) for rightwalks in rightwalk]
+        leftstatic = [pygame.transform.flip(rightstatic[0], True, 0)]
+        leftdead = [pygame.transform.flip(rightdead[0], True, 0)]
+        leftattack = [pygame.transform.flip(rightattack[0], True, 0)]
+        leftgetattack = [pygame.transform.flip(rightgetattack[0], True, 0)]
+
+        self.rightlist = [rightstatic, rightwalk, rightattack, rightgetattack ,rightdead]
+        self.leftlist = [leftstatic, leftwalk, leftattack, leftgetattack ,leftdead]
+        self.curlist = self.leftlist
+        self.cursprite = self.curlist[self.cur][self.index]
+        self.hitbox = self.cursprite.get_rect(topleft=(self.x_pos, self.y_pos))
+        
+    def SetStat(self, HP, ATK, DEF, SPEED):
+        self.HP = HP
+        self.ATK = ATK
+        self.DEF = DEF
+        self.SPEED = SPEED
+        
+    def ChangeStat(self, HP=0, ATK=0, DEF=0, SPEED=0):
+        self.HP += HP
+        self.ATK += ATK
+        self.DEF += DEF
+        self.SPEED += SPEED
+        
+    def checkcollision(self, Anathor):
+        if (pygame.Rect.colliderect(self.hitbox, Anathor.hitbox)):
+            return True
+        else:
+            return False
+    
+    def AI(self, player):
+        distance = self.hitbox.centerx - player.hitbox.centerx
+        dx1 = int(self.hitbox.width / 2)
+        dx2 = int(player.hitbox.width / 2)
+        if (distance > 0):
+            self.direction = LEFT
+            self.isAttack = False
+            self.isWalk = True
+        else:
+            self.direction = RIGHT
+            self.isAttack = False
+            self.isWalk = True
+        
+        if (abs(distance) <= dx1 + dx2):
+            self.isAttack = True
+            self.isWalk = False
+        
+        for projectile in player.projectilelist: #플레이어 내에 변수에 직접 접근은 최대한 피하고 싶은데.... 방법 x?
+            if (len(player.projectilelist) != 0):
+                if (self.isHitbox is True):
+                    if (self.checkcollision(projectile) is True):
+                        self.isGetattack = True
+                        self.isWalk = False
+                        self.isAttack = False
+                        self.HP -= (projectile.ATK - self.DEF)
+                        player.projectilelist.remove(bubble)
+                        if (self.x_pos > player.x_pos):
+                            self.direction = LEFT
+                            self.x_pos += self.GETATTACK
+                        else:
+                            self.direction = RIGHT
+                            self.x_pos -= self.GETATTACK
+                        
+        if (self.isDead is True):
+            self.SPEED = 0
+            self.GETATTACK = 0
+            self.isHitbox = False
+
+    def drawpos(self):
+        Screen.blit(self.cursprite, (self.hitbox.x, self.hitbox.y))
+        
+    def drawStat(self):
+        Length = self.HP / 20
+        DisplayLength = 2500 / 20
+        pygame.draw.rect(Screen, RED, (self.hitbox.centerx - DisplayLength / 2,
+                                       self.hitbox.bottom + 18, DisplayLength, 12), 1)
+        if (self.HP >= 0):
+            pygame.draw.rect(Screen, RED, (self.hitbox.centerx - DisplayLength / 2,
+                                           self.hitbox.bottom + 19, Length, 10))
+            
+    def draw(self):
+        self.drawpos()
+        self.drawStat()
+    
+    def updatepos(self):
+        self.hitbox.x = self.x_pos
+        self.hitbox.y = self.y_pos
+
+        if (self.isOnGround is False):
+            self.y_pos += GRAVITY
+        if (self.hitbox.bottom >= MAP_GROUND):
+            self.y_pos = MAP_GROUND - self.hitbox.height
+            self.isOnGround = True
+        if (self.hitbox.left <= MAP_LIMIT_LEFT):
+            self.x_pos = MAP_LIMIT_LEFT
+        if (self.hitbox.right >= MAP_LIMIT_RIGHT):
+            self.x_pos = MAP_LIMIT_RIGHT - self.hitbox.width
+            
+        if (self.isWalk is True and self.isAttack is False):
+            if (self.direction == LEFT):
+                self.x_pos += -self.SPEED
+            elif (self.direction == RIGHT):
+                self.x_pos += self.SPEED
+    
+    def updatesprite(self):
+        if (self.direction == LEFT):
+            self.curlist = self.leftlist
+        elif (self.direction == RIGHT):
+            self.curlist = self.rightlist
+        
+        if (self.isWalk is False or self.isAttack is False or self.isDead is False):
+            self.cur = 0
+        if (self.isWalk is True):
+            self.cur = 1
+        if (self.isAttack is True):
+            self.cur = 2
+        if (self.isGetattack is True):
+            self.cur = 3
+        if (self.HP <= 0):
+            self.cur = 4
+            self.isDead = True
+
+        self.index += 1
+        if (self.index >= len(self.curlist[self.cur])):
+            self.index = 0
+        
+        self.cursprite = self.curlist[self.cur][self.index]
+        
+    def update(self):
+        self.updatepos()
+        self.updatesprite()
 
 class Boss(object):
     def __init__(self):
         pass
-
-pygame.init() # pygame 초기화
 
 font = pygame.font.SysFont('굴림', 40)
 def write(Text, color, x_pos, y_pos):
@@ -229,14 +403,12 @@ Clock = pygame.time.Clock()
 mapimage = pygame.image.load('display.png')
 mapscale = pygame.transform.scale(mapimage, (800, 600))
 
-player = Player(300, MAP_GROUND - 64)
-enemy = Player(500, MAP_GROUND - 64)
-Bubblelist = []
-Enemylist = []
+Itemlist = ['ICE', 'ARMOR', 'HASTE', 'DOUBLE'] #-> 전역변수화
+player = Player(300, 300)
+player.InitPos()
+Enemylist = [Enemy(600, MAP_GROUND)]
 
 while True:
-    Screen.blit(mapscale, (0, 0))
-
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             pygame.quit()
@@ -263,28 +435,20 @@ while True:
 
             elif (event.key == pygame.K_x):
                 player.notAttack()
-    
-    for bubble in player.projectilelist:
-        if (bubble.checkcollision(enemy) is True):
-            if (bubble.ATK > enemy.DEF):
-                enemy.ChangeStat(-(bubble.ATK - enemy.DEF))
-                enemy.getattack()
-            else:
-                pass
-            if (enemy.isDead is not True):
-                player.getProjectils().remove(bubble)
-            
+                
+    Screen.blit(mapscale, (0, 0))
     player.draw()
-    enemy.draw()
-    player.drawStat()
-    player.updatepos()
-    player.updatesprite()
-    enemy.updatepos()
-    enemy.updatesprite()
-    if (len(player.getProjectils()) != 0):
-        for bubble in player.getProjectils():
+    player.update()
+    if (len(player.projectilelist) != 0):
+        for bubble in player.projectilelist:
             bubble.move()
             bubble.draw()
-    write(str(len(player.getProjectils())), BLACK, 400, 20)
+            
+    for enemy in Enemylist:
+        if (len(Enemylist) != 0):
+            enemy.AI(player)
+            enemy.draw()
+            enemy.update()
+    write(str(enemy.isDead), BLACK, 400, 20)
     pygame.display.update()
     Clock.tick(FPS)
